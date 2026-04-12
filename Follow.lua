a75d0dc4e39e83a3bb33840827e9f6939068d860
kill9512 <88079302+kill9512@users.noreply.github.com>
2026-04-12 23:35:50 +0700

    Update Follow.lua

--- Follow.lua
+++ Follow.lua
@@ -2,4 +2,4 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("KONG GUISUS", "DarkScene")
 local MainTab = Window:NewTab("Main")
-local MainSection = MainTab:NewSection("Follwer Player")
+local MainSection = Tab:NewSection("Follwer Player")
